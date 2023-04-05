a1ec783e0c4c57ccda04afc3a6ae13aa28fbe96a
mateus83 <127039861+mateus83@users.noreply.github.com>
2023-04-05 00:39:25 -0300

    Update graficos.st.py

--- graficos.st.py
+++ graficos.st.py
@@ -84,21 +84,16 @@ mpl.rcParams['figure.figsize'] = [6, 4] # Define o tamanho da figura
 mpl.rcParams['font.size'] = 10 # Define o tamanho da fonte
 
 # Criando o gráfico de pizza
-fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(10, 6))
-for i, (item, data) in enumerate(dados_agrupados.items()):
-    row = i // 3
-    col = i % 3
-    axes[row, col].pie(data, labels=data.index, autopct='%1.1f%%')
-    axes[row, col].set_title(item, fontsize=10)
-plt.tight_layout()
-
+plt.figure(figsize=(8, 6))
+fig, ax = plt.subplots()
+ax.pie(dados_agrupados, labels=dados_agrupados.index, autopct='%1.1f%%')
 
 # Contando a quantidade de vezes que a palavra "ENTREGUE" aparece na coluna "Sites entregue"
 count_entregue = len(grupo_selecionado[grupo_selecionado['Sites entregue'] == 'ENTREGUE'])
 
 # Adicionando o subtítulo ao título do gráfico
 dados_agrupados_str = [f"{item[0]}: {item[1]}" for item in dados_agrupados.items()]
-axes[1, 0].set_title(f"Contagem de {coluna_selecionada.rsplit(' ', 1)[0]} {titulo} - {mes_ano_selecionado}\n{count_entregue} Sites entregue - {' - '.join(dados_agrupados_str)}", fontsize=12)
+ax.set_title(f"Contagem de {coluna_selecionada.rsplit(' ', 1)[0]} {titulo} - {mes_ano_selecionado}\n{count_entregue} Sites entregue - {' - '.join(dados_agrupados_str)}", fontsize=12)
 
 # Exibindo o gráfico
 plt.tight_layout()
